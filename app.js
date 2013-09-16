5c9da9b2d12236ccd221c558d02435a03c2001e0
Bret <none>
2013-09-16 14:55:31 -0500

    Create the app core file.

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-
 /**
  * Module dependencies.
  */
-
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
 
 var app = express();
 
-// all environments
+// Configure expresss environments.
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
