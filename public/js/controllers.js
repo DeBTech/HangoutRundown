7e312775fee62a77e814e3cbabb1d2113791dbd6
Bret <bret@corthon.com>
2013-09-18 23:00:29 -0500

    Implemented initial controller methods to add, regress, and advance topics.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,14 +3,32 @@
 /* Controllers */
 
 function HangDownListCntr($scope) {
-  $scope.items = [
-    {label: 'Let us discuss this.', creator: 'Bret'},
-    {label: 'ALL discuss this.', creator: 'D'},
-    {label: 'More discuss this.', creator: 'Bret'},
-    {label: 'ALL more discuss this.', creator: 'D'}
-  ];
+  $scope.items = [];
 
   $scope.activeItem = 1;
+  $scope.currentUser = 'Bret';
+
+  $scope.regressTopic = function(){
+    if ($scope.activeItem > 0)
+      $scope.activeItem--;
+  };
+
+  $scope.advanceTopic = function(){
+    if ($scope.activeItem < $scope.items.length - 1)
+      $scope.activeItem++;
+  };
+
+  $scope.newTopicBuffer = '';
+  $scope.addNewTopic = function(){
+    // If there is no topic set, bail.
+    if (!$scope.newTopicBuffer.length) return;
+
+    // Otherwise, add the topic and reset the buffer.
+    $scope.items.push({ label: $scope.newTopicBuffer, creator: $scope.currentUser });
+    $scope.newTopicBuffer = '';
+
+    // TODO: Create a model to encapsulate the list and notifies Google of changes.
+  };
 }
 
 //HangDownListCntr.$inject = ['$scope'];
--- test/angular_unit/controllerSpec.js
+++ test/angular_unit/controllerSpec.js
@@ -14,5 +14,63 @@ describe('HangDown Controllers', function() {
     it('should have a list of items', function() {
       expect(scope.items).toBeDefined();
     });
+
+    it('should be able to add new topics', function(){
+      expect(scope.newTopicBuffer).toBeDefined();
+      expect(scope.items.length).toEqual(0);
+
+      scope.newTopicBuffer = 'This is my new topic';
+      scope.addNewTopic();
+
+      expect(scope.items.length).toEqual(1);
+      expect(scope.newTopicBuffer).toEqual('');
+    });
+
+    it('should not add empty topics', function(){
+      scope.newTopicBuffer = '';
+      scope.addNewTopic();
+
+      expect(scope.items.length).toEqual(0);
+    });
+
+    var sampleTopics = [
+      { label: 'This is a thing.', creator: 'Bret' },
+      { label: 'This is another thing.', creator: 'Alicia' },
+      { label: 'This is a third thing.', creator: 'Frank' },
+      { label: 'This is NOT a thing.', creator: 'Jim' },
+      { label: 'This is an after thing.', creator: 'Jan' }
+    ];
+
+    it('should be able to advance topics', function(){
+      scope.items = sampleTopics;
+      scope.activeItem = 2;
+
+      scope.advanceTopic();
+      expect(scope.activeItem).toEqual(3);
+    });
+
+    it('should not advance past the end of the topic list', function(){
+      scope.items = sampleTopics;
+      scope.activeItem = scope.items.length - 1;
+
+      scope.advanceTopic();
+      expect(scope.activeItem).toEqual(scope.items.length - 1);
+    });
+
+    it('should be able to regress topics', function(){
+      scope.items = sampleTopics;
+      scope.activeItem = 2;
+
+      scope.regressTopic();
+      expect(scope.activeItem).toEqual(1);
+    });
+
+    it('should not regress past the beginning of the topic list', function(){
+      scope.items = sampleTopics;
+      scope.activeItem = 0;
+
+      scope.regressTopic();
+      expect(scope.activeItem).toEqual(0);
+    });
   });
 });
