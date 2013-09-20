0ee157b539743d632ab6187407fb292081276459
Bret <bret@corthon.com>
2013-09-19 22:13:21 -0500

    First attempt at a full integration of gAPI.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,19 +3,35 @@
 /* Controllers */
 
 function HangDownListCntr($scope) {
+  $scope.apiLive = false;
+
   $scope.items = [];
 
   $scope.activeItem = 0;
   $scope.currentUser = null;
 
   $scope.regressTopic = function(){
-    if ($scope.activeItem > 0)
+    // If the API is not up yet, bail.
+    if (!$scope.apiLive) return;
+
+    if ($scope.activeItem > 0) {
       $scope.activeItem--;
+
+      // Submit changes to Google.
+      gapi.hangout.data.submitDelta( { activeItem: $scope.activeItem } );
+    }
   };
 
   $scope.advanceTopic = function(){
-    if ($scope.activeItem < $scope.items.length - 1)
+    // If the API is not up yet, bail.
+    if (!$scope.apiLive) return;
+    
+    if ($scope.activeItem < $scope.items.length - 1) {
       $scope.activeItem++;
+
+      // Submit changes to Google.
+      gapi.hangout.data.submitDelta( { activeItem: $scope.activeItem } );
+    }
   };
 
   $scope.newTopicBuffer = '';
@@ -30,12 +46,22 @@ function HangDownListCntr($scope) {
     $scope.items.push({ label: $scope.newTopicBuffer, creator: $scope.currentUser });
     $scope.newTopicBuffer = '';
 
-    // TODO: Create a model to encapsulate the list and notifies Google of changes.
+    // TODO: Create a model to encapsulate the list and notify Google of changes.
+
+    // Submit changes to Google.
+    gapi.hangout.data.submitDelta( { topics: JSON.stringify($scope.items) } );
+  };
+
+  var updateSharedState = function(StateChangedEvent){
+    $scope.items = JSON.parse(StateChangedEvent.state.topics);
+    $scope.activeItem = StateChangedEvent.state.activeItem;
   };
 
   // Add a callback to initialize gAPI elements.
   gapi.hangout.onApiReady.add(function(eventObj){
+    $scope.apiLive = true;
     $scope.currentUser = gapi.hangout.getLocalParticipant().person.displayName;
+    gapi.hangout.data.onStateChanged.add(updateSharedState);
   });
 }
 
